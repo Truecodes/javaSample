1e9991f5f443859faa0a5e05f38d6ba8ccb4c4c8
Kyle <aretae@gmail.com>
2018-04-12 14:09:50 -0400

    added selenium tests

---
+++ src/test/java/selenium/BasicPageTest.java
@@ -0,0 +1,33 @@
+package selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by wasadmin on 4/12/2018.
+ */
+public class BasicPageTest {
+
+    @Test
+    public void testThatPageOpens() {
+        String pathToDriver = "c:/drivers/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://www.google.com");
+        driver.findElement(By.id("lst-ib")).sendKeys("selenium");
+        driver.findElement(By.className("lsb")).click();
+        driver.findElement(By.linkText("Selenium - Web Browser Automation")).click();
+        assertEquals("Selenium - Web Browser Automation", driver.getTitle());
+
+    }
+}
